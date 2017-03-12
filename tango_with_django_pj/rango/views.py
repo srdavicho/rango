1d0140b7b75c0ef740a29d857442485d4f5314bd
David Gaudreault <david.gaudreault@gmail.com>
2017-03-12 17:26:07 -0400

    static and media file examples

--- tango_with_django_pj/rango/views.py
+++ tango_with_django_pj/rango/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+# def index(request):
+# 	return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about'>About</a>")
+
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+	return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+	
+	context = {
+		'myname': "David Gaudreault",
+	}
+
+	return render(request, 'rango/about.html', context=context)
