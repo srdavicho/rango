5f19ab8a418606b482452441bfe53989a9ae214d
David Gaudreault <david.gaudreault@gmail.com>
2017-03-29 06:08:58 -0400

    working project

--- tango_with_django_pj/rango/models.py
+++ tango_with_django_pj/rango/models.py
@@ -1,27 +1,37 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-	name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
 
-	class Meta:
-		verbose_name_plural = 'Categories'
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
-	def __unicode__(self):
-		return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
 
-	def __str__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 
 class Page(models.Model):
-	category = models.ForeignKey(Category)
-	title = models.CharField(max_length=128)
-	url = models.URLField()
-	views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
 
-	def __unicode__(self):
-		return self.title
 
-	def __str__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
--- tango_with_django_pj/rango/views.py
+++ tango_with_django_pj/rango/views.py
@@ -1,19 +1,88 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 # def index(request):
 # 	return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about'>About</a>")
 
-def index(request):
-	
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-
-	return render(request, 'rango/index.html', context=context_dict)
-
 def about(request):
-	
 	context = {
 		'myname': "David Gaudreault",
 	}
 
 	return render(request, 'rango/about.html', context=context)
+
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method =='POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			cat = form.save(commit=True)
+			print(cat, cat.slug)
+			return index(request)
+		else:
+			print(form.errors)
+	return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except:
+		category = None
+
+	form = PageForm()
+	if request.method == "POST":
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+
+	context_dict = {'form':form, 'category':category}
+	return render(request, 'rango/add_page.html', context_dict)
+
+def index(request):
+	
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages':page_list}
+
+	return render(request, 'rango/index.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+	# Create a context dictionary which we can pass
+	# to the template rendering engine.
+	context_dict = {}
+
+	try:
+		# Can we find a category name slug with the given name?
+		# If we can't, the .get() method raises a DoesNotExist exception.
+		# So the .get() method returns one model instance or raises an exception.
+		category = Category.objects.get(slug=category_name_slug)
+
+		# Retrieve all of the associated pages.
+		# Note that filter() will return a list of page objects or an empty list
+		pages = Page.objects.filter(category=category)
+
+		# Adds our results list to the template context under name pages.
+		context_dict['pages'] = pages
+		# We also add the category object from
+		# the database to the context dictionary.
+		# We'll use this in the template to verify that the category exists.
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		# We get here if we didn't find the specified category.
+		# Don't do anything -
+		# the template will display the "no category" message for us.
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	# Go render the response and return it to the client.
+	return render(request, 'rango/category.html', context_dict)
